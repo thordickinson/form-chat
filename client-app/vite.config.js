0ef1f7c37e50bd9a4a30fbbcfb318be2f6d12307
Dickinson Arismendy <thordickinson@gmail.com>
2024-06-10 10:47:28 -0500

    added vite proxy

--- client-app/vite.config.js
+++ client-app/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:4000'
+    }
+  }
 })
