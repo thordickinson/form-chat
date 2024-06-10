6cca36ed80502ad570ba36588154bd70b39ccf6f
Dickinson Arismendy <thordickinson@gmail.com>
2024-06-09 22:17:15 -0500

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,8 @@ import PushToTalk from './components/PushToTalk';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello, React!</h1>
+    <div className="App p-4">
+      <h1 className="text-2xl font-bold">Hello, React!</h1>
       <PushToTalk />
     </div>
   );
