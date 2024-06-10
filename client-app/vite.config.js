64f298c45127c02d871fee7998a61611f55a843e
Dickinson Arismendy <thordickinson@gmail.com>
2024-06-10 10:49:09 -0500

    fix uploads and use default vite port

--- client-app/vite.config.js
+++ client-app/vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
     host: '0.0.0.0',
     proxy: {
       '/api': 'http://localhost:4000'
